bf3c1bc9d391649a6a2faf1ac7c730973cf106ef
Vladyslav Poltavets <vpoltavets02@gmail.com>
2023-10-08 20:34:29 +0300

    Initial commit

---
+++ src/main/java/com/social/network/mysn/MySnApplication.java
@@ -0,0 +1,13 @@
+package com.social.network.mysn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/network/mysn/MySnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.network.mysn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
